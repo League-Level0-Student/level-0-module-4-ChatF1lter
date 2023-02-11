e452b5b7ff98ad00d9c2c71886c348358684d3eb
league <league@imac-32.attlocal.net>
2023-02-11 12:56:48 -0800

    ChangeCalculator(IP)

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,15 +12,15 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels  = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int nickelsNumber = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
 		// Ask the user how many quarters they have, and convert their answer
-
+int dimesNumber = Integer.parseInt(dimes);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+ 
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
 
 	}
