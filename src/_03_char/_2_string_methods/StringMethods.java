139ec06f537540847df47c460b43d4be10594e48
league <league@imac-16.attlocal.net>
2023-04-22 12:57:05 -0700

    StringMethods(IP)

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -6,18 +6,22 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-String yes = "yes";
+String code = "code";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+System.out.println(code.charAt(2));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
+System.out.println(code.length());
 
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(System.out.println(code.length()){
+	System.out.println(code.charAt(ran));
+}
+	
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
